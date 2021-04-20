8b1e6c2fc02fc33b3db5ac853ce9e0688f9be40e
alegm9800 <201722065@tese.edu.mx>
2021-04-20 14:55:40 -0500

    Ver 1.00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { HeroesModule } from './heroes/heroes.module'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+
+  declarations: [AppComponent],
+  imports: [BrowserModule,AppRoutingModule, HeroesModule, ContadorModule],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
+
 export class AppModule { }
